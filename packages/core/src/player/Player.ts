31024d2ff59d43dda4f089a1d20a4f7adbb49943
MarckVinny <mvrpj@msn.com>
2023-09-22 22:15:11 -0300

    Criando a Classe Player

---
+++ packages/core/src/player/Player.ts
@@ -0,0 +1,22 @@
+import { PlayerType } from '../../shared/PlayerType'
+
+export default class Player {
+  constructor(
+    readonly name: string,
+    readonly type: PlayerType,
+    readonly score: number = 0
+  ) {}
+
+  addScore(score: number){
+    if(score === 0) return this
+    return new Player(
+        this.name,
+        this.type,
+        this.score + score
+    )
+  }
+
+  clear(): Player {
+    return new Player(this.name, this.type, 0)
+  }
+}
