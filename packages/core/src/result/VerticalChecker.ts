860ef8fe3896480a8b62fc17d311e2364da7baaf
MarckVinny <mvrpj@msn.com>
2023-10-11 23:10:48 -0300

    Criando o Teste Unitário da Classe VerticalChecker correção

--- packages/core/src/result/VerticalChecker.ts
+++ packages/core/src/result/VerticalChecker.ts
@@ -5,9 +5,9 @@ import ResultChecker from './ResultChecker'
 
 export default class VerticalChecker implements ResultChecker {
   check(board: Board): GameResult {
-    const coluna0: [number, number][] = [[0, 0], [0, 1], [0, 2]] //* posições da Coluna0
-    const coluna1: [number, number][] = [[1, 0], [1, 1], [1, 2]] //* posições da Coluna1
-    const coluna2: [number, number][] = [[2, 0], [2, 1], [2, 2]] //* posições da Coluna2
+    const coluna0: [number, number][] = [[0, 0], [1, 0], [2, 0]] //* posições da Coluna0
+    const coluna1: [number, number][] = [[0, 1], [1, 1], [2, 1]] //* posições da Coluna1
+    const coluna2: [number, number][] = [[0, 2], [1, 2], [2, 2]] //* posições da Coluna2
     const finalResult = [
       //* Verifica se todas as Células da Coluna estão preenchidas com o mesmo Jogador
       //* depois o board verifica se houve alguma Jogada Vencedora na Coluna
