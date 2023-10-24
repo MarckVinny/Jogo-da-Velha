ab8e80c41ae1b6a5f36b32119f69d9686fcf62b4
MarckVinny <mvrpj@msn.com>
2023-10-24 14:46:34 -0300

    Configurando o TailwindCSS

--- apps/frontend/tailwind.config.ts
+++ apps/frontend/tailwind.config.ts
@@ -1,20 +1,37 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      primary: {
+        400: '#65E9E4',
+        500: '#31C3BD',
+        600: '#218C87',
+      },
+      secondary: {
+        400: '#FCC860',
+        500: '#F3B136',
+        600: '#CC8B13',
+      },
+      light: {
+        400: '#DBE8ED',
+        500: '#A8BFC9',
+        600: '#6B8997',
+      },
+      Dark: {
+        400: '#1F3641',
+        500: '#1F3641',
+        600: '#10212A',
       },
     },
   },
+  safelist: [
+    { pattern: /^bg-primary/, variants: [ 'hover' ] },
+    { pattern: /^bg-secondary/, variants: [ 'hover' ] },
+    { pattern: /^bg-light/, variants: [ 'hover' ] },
+    { pattern: /^bg-dark/, variants: [ 'hover' ] },
+  ],
   plugins: [],
 }
 export default config
