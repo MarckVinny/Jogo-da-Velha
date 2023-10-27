21dfe89c8f0ea999379ef1b0d01f23025f359bba
MarckVinny <mvrpj@msn.com>
2023-10-27 17:18:08 -0300

    correção

--- apps/frontend/tailwind.config.ts
+++ apps/frontend/tailwind.config.ts
@@ -4,33 +4,35 @@ const config: Config = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
-      primary: {
-        400: '#65E9E4',
-        500: '#31C3BD',
-        600: '#218C87',
-      },
-      secondary: {
-        400: '#FCC860',
-        500: '#F3B136',
-        600: '#CC8B13',
-      },
-      light: {
-        400: '#DBE8ED',
-        500: '#A8BFC9',
-        600: '#6B8997',
-      },
-      Dark: {
-        400: '#1F3641',
-        500: '#1F3641',
-        600: '#10212A',
+      colors: {
+        primary: {
+          400: '#65E9E4',
+          500: '#31C3BD',
+          600: '#218C87',
+        },
+        secondary: {
+          400: '#FCC860',
+          500: '#F3B136',
+          600: '#CC8B13',
+        },
+        light: {
+          400: '#DBE8ED',
+          500: '#A8BFC9',
+          600: '#6B8997',
+        },
+        dark: {
+          400: '#1F3641',
+          500: '#1F3641',
+          600: '#10212A',
+        },
       },
     },
   },
   safelist: [
-    { pattern: /^bg-primary/, variants: [ 'hover' ] },
-    { pattern: /^bg-secondary/, variants: [ 'hover' ] },
-    { pattern: /^bg-light/, variants: [ 'hover' ] },
-    { pattern: /^bg-dark/, variants: [ 'hover' ] },
+    { pattern: /^bg-primary/, variants: ['hover'] },
+    { pattern: /^bg-secondary/, variants: ['hover'] },
+    { pattern: /^bg-light/, variants: ['hover'] },
+    { pattern: /^bg-dark/, variants: ['hover'] },
   ],
   plugins: [],
 }
