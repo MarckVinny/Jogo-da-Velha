aeb1ac1f3c8e516fbe7fcb7d955fead0f20a68d3
MarckVinny <mvrpj@msn.com>
2023-10-29 02:19:18 -0300

    Componente Card - Font-end

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,7 +1,24 @@
-import Card from '@/Components/shared/Card'
+import Card from "@/components/shared/Card"
 
 export default function Home() {
-  return <Card color='primary'>
-    isto é um Card!
-  </Card>
+  return (
+    <div className='flex justify-around text-center mt-10'>
+      <Card color='primary'>
+        <h2 className='text-lg'>Isto é um Card!</h2>
+        <p className='text-sm'>com Cor Primária</p>
+      </Card>
+      <Card color='secondary'>
+        <h2 className='text-lg'>Isto é um Card!</h2>
+        <p className='text-sm'>com Cor Secundária</p>
+      </Card>
+      <Card color='light'>
+        <h2 className='text-lg'>Isto é um Card!</h2>
+        <p className='text-sm'>com Cor Light</p>
+      </Card>
+      <Card color='dark'>
+        <h2 className='text-lg'>Isto é um Card!</h2>
+        <p className='text-sm'>com Cor Dark</p>
+      </Card>
+    </div>
+  )
 }
