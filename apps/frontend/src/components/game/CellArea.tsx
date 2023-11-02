a9234505364adc946faa0be9a5ee3b041386160c
MarckVinny <mvrpj@msn.com>
2023-11-02 17:18:07 -0300

    Componente `<CellArea>`

---
+++ apps/frontend/src/components/game/CellArea.tsx
@@ -0,0 +1,47 @@
+import { IconCircle, IconX } from '@tabler/icons-react'
+import { PlayerType } from 'core'
+import Card from '../shared/Card'
+
+export interface CellAreaProps {
+  type?: PlayerType | null
+  selected?: boolean
+}
+
+export default function CellArea(props: CellAreaProps) {
+  return (
+    <Card                                 //* Componente Card
+      color={                             //* Interface de Cor do Componente Card
+        !props.selected                   //* Se NÃO estiver selecionado,
+          ? 'dark'                        //* Então, use a cor Dark 
+          : props.type === PlayerType.X   //* Senão, verifique Se o Tipo é estritamente igual ao Tipo do "Jogador X"
+          ? 'primary'                     //* Então, use a cor Primária 
+          : 'secondary'                   //* Senão, use a cor Secundária
+        }
+    >
+      <div className={`flex justify-center items-center w-12 h-12`}>
+        {props.type === PlayerType.X && (
+          <IconX
+            size={70}
+            stroke={6}
+            className={
+              props.selected
+                ? 'text-dark-500'
+                : 'text-primary-500'
+            }
+          />
+        )}
+        {props.type === PlayerType.O && (
+          <IconCircle
+            size={70}
+            stroke={6}
+            className={
+              props.selected
+                ? 'text-dark-500'
+                : 'text-secondary-500'
+            }
+          />
+        )}
+      </div>
+    </Card>
+  )
+}
